85a8d81a55ad0b822be3f38d7cf3c678b4415ceb
OmarMWarraich <earthling986@gmail.com>
2024-03-05 16:26:51 +0500

    feat: Logs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 
-setup (
-    name= "mcq-generator",
-    version= "0.0.1",
-    author= "Omar M. Warraich",
-    author_email= "omarwarraich@hotmail.com",
-    install_requires = ["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
-    packages= find_packages()
+setup(
+    name="mcq-generator",
+    version="0.0.1",
+    author="Omar M. Warraich",
+    author_email="omarwarraich@hotmail.com",
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages(),
 )
